ce1e73a69433a0dfae97578cf5372b5c046c309d
kctxdev <johnataichigo65@gmail.com>
2026-04-16 16:58:04 -0300

    fix: alinhamento final de sintaxe para o terraform fmt

--- main.tf
+++ main.tf
@@ -8,19 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  region = "sa-east-1" # Região de São Paulo
+  region = "sa-east-1"
 }
 
-# 1. Criando o Cofre (Secrets Manager) na AWS
 resource "aws_secretsmanager_secret" "meu_cofre_seguro" {
-  name        = "senha-banco-de-dados-producao"
-  description = "Cofre criado via Pipeline CI/CD automática"
-
-  # Isso evita que o segredo fique 'pendurado' por 30 dias se deletarmos
+  name                    = "senha-banco-de-dados-producao"
+  description             = "Cofre criado via Pipeline CI/CD automática"
   recovery_window_in_days = 0
 }
 
-# 2. Guardando uma senha (exemplo) dentro do cofre
 resource "aws_secretsmanager_secret_version" "senha_cofre" {
   secret_id     = aws_secretsmanager_secret.meu_cofre_seguro.id
   secret_string = "P@ssw0rd_Segur4_2026!"
